28d34ec6935f0e3a7d927ea095038d96af8cc32d
PadPalon <luca.neukom@gmail.com>
2016-02-28 16:14:54 +0100

    add documentatio to List

--- src/ch/neukom/learning/data/List.java
+++ src/ch/neukom/learning/data/List.java
@@ -7,18 +7,22 @@ public class List {
 	public static void main(String[] args) {
 	}
 	
+	/**
+	 * List sind Kollektionen von Daten. Die Daten sind geordnet , erlauben Duplikate und können null sein. 
+	 */
 	private class Example {
 		/**
-		 * 
+		 * Werte können an jeden Ort einer Liste angehängt werden
 		 */
 		private void addList() {
 			ArrayList<String> strings = new ArrayList<String>();
 			strings.add("first");
 			strings.add("second");
+			strings.add(0, "third");
 		}
 		
 		/**
-		 * 
+		 * Werte können aus einer Liste wieder entfernt werden
 		 */
 		private void removeList() {
 			ArrayList<String> strings = new ArrayList<String>();
